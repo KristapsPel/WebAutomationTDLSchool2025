bbd53997fe4645dc2c83adfa60be8b058bf24caa
Kristaps Pelcbergs <v-krpelc@microsoft.com>
2025-08-14 11:24:19 +0300

    Add NavigationTest

--- src/test/java/testScripts/TDLSchool/NavigationTest.java
+++ src/test/java/testScripts/TDLSchool/NavigationTest.java
@@ -1,34 +1,57 @@
 package testScripts.TDLSchool;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import java.io.File;
 
 public class NavigationTest {
+    private WebDriver driver;
+
+    @AfterMethod
+    private void tearDown () {
+        this.driver.close();
+        this.driver.quit();
+    }
 
     @Test
-    public void openTDLSchoolPage () {
+    public void openTDLSchoolPage () throws InterruptedException {
 
 //        WebDriver driver = setUpWebDriverManually("chrome");
 //        System.setProperty("wdm.edgeDriverUrl", "https://msedgedriver.microsoft.com/");
-        WebDriver driver = setUpWebDriverManage("edge");
-        driver.manage().window().maximize();
-        driver.get("https://tdlschool.com/");
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        this.driver = setUpWebDriverManage("edge");
+        this.driver.manage().window().maximize();
+        this.driver.get("https://tdlschool.com/");
+        Thread.sleep(2000);
+
+        WebElement logo = driver.findElement(By.className("navigation__logo"));
+        WebElement buttonOurCourses= driver.findElement(By.linkText("Our courses"));
+        Assert.assertTrue(logo.isDisplayed(), "Logo is not displayed on page");
+        Assert.assertTrue(buttonOurCourses.isDisplayed(), "Button is not displayed on page");
+        WebElement careerPathsLink = driver.findElement(By.linkText("Career Paths"));
+        careerPathsLink.click();
+        WebElement careerPathTitle = driver.findElement(By.tagName("h1"));
+        Assert.assertEquals(careerPathTitle.getText(),
+                "Not sure where to start?",
+                "Title is not as expected");
+        System.out.println("Title:"+careerPathTitle.getText());
+        WebElement emailInputField= driver.findElement(By.name("email"));
+        Assert.assertTrue(emailInputField.isDisplayed(), "Email input is not Displayed");
+        emailInputField.sendKeys("test");
+        Thread.sleep(1000);
+
+        Thread.sleep(2000);
         System.out.println("Title:"+ driver.getTitle());
         System.out.println("URL:"+ driver.getCurrentUrl());
-        driver.close();
-        //driver.quit();
     }
     private WebDriver setUpWebDriverManage (String browser) {
         WebDriver driver = null;

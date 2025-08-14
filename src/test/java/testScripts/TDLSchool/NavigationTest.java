4e69f21bae08efa31d8355dabc7519c145b2a0db
Kristaps Pelcbergs <v-krpelc@microsoft.com>
2025-08-14 13:58:59 +0300

    Add Extent report

--- src/test/java/testScripts/TDLSchool/NavigationTest.java
+++ src/test/java/testScripts/TDLSchool/NavigationTest.java
@@ -1,9 +1,12 @@
 package testScripts.TDLSchool;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -14,6 +17,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.lang.reflect.Method;
 
 public class NavigationTest {
     private WebDriver driver;
@@ -31,32 +35,65 @@ public class NavigationTest {
         this.driver.get("https://tdlschool.com/");
     }
 
-    @Test
-    public void openTDLSchoolPage() throws InterruptedException {
+    @Test (testName = "Navigate to Career Paths page",
+            description = "We are opening TDL School page and entering text in footer")
+    public void openTDLSchoolPage(Method method) throws InterruptedException {
+        ExtentSparkReporter sparkReporter = new ExtentSparkReporter(
+                System.getProperty("user.dir") +
+                        File.separator + "reports" +
+                        File.separator + "TestReport.html");
+        ExtentReports reports = new ExtentReports();
+        reports.attachReporter(sparkReporter);
 
-//        WebDriver driver = setUpWebDriverManually("chrome");
-//        System.setProperty("wdm.edgeDriverUrl", "https://msedgedriver.microsoft.com/");
+        String testName = method.getAnnotation(Test.class).testName();
+        String description = method.getAnnotation(Test.class).description();
+
+        ExtentTest test = reports.createTest(testName, description);
         Thread.sleep(2000);
 
+        test.log(Status.INFO, "Check that logo is Displayed");
         WebElement logo = driver.findElement(By.className("navigation__logo"));
-        WebElement buttonOurCourses = driver.findElement(By.linkText("Our courses"));
         Assert.assertTrue(logo.isDisplayed(), "Logo is not displayed on page");
-        Assert.assertTrue(buttonOurCourses.isDisplayed(), "Button is not displayed on page");
+        test.log(Status.PASS, "Logo is displayed");
+
+        test.log(Status.INFO, "Click on Career Paths button");
         WebElement careerPathsLink = driver.findElement(By.linkText("Career Paths"));
         careerPathsLink.click();
+        test.log(Status.PASS, " Click action on Career path did work");
+
+        test.log(Status.INFO, "Title Not sure where to start? is displayed");
         WebElement careerPathTitle = driver.findElement(By.tagName("h1"));
         Assert.assertEquals(careerPathTitle.getText(),
                 "Not sure where to start?",
                 "Title is not as expected");
-        System.out.println("Title:" + careerPathTitle.getText());
+        test.log(Status.PASS, "Title "+ careerPathTitle.getText()+" was displayed correctly");
+
+        test.log(Status.INFO, "Enter email in email field");
         WebElement emailInputField = driver.findElement(By.name("email"));
         Assert.assertTrue(emailInputField.isDisplayed(), "Email input is not Displayed");
         emailInputField.sendKeys("test");
+        test.log(Status.PASS, "Text entered in email field");
+
         Thread.sleep(1000);
 
         Thread.sleep(2000);
-        System.out.println("Title:" + driver.getTitle());
-        System.out.println("URL:" + driver.getCurrentUrl());
+        test.log(Status.INFO, "Title:" + driver.getTitle());
+        test.log(Status.INFO, "URL:" + driver.getCurrentUrl());
+
+        addScreenshotToReport(Status.PASS, test);
+
+        reports.flush();
+
+
+
+    }
+
+    private void addScreenshotToReport (Status status, ExtentTest test){
+        // Capture the screenshot as a Base64 string and prepend with the data URI scheme for a PNG image
+        String base64ScreenShot = "data:image/png;base64," + ((TakesScreenshot) driver).getScreenshotAs(OutputType.BASE64);
+
+        // Log the screenshot to the report with the specified status (PASS, FAIL, etc.)
+        test.log(status, MediaEntityBuilder.createScreenCaptureFromBase64String(base64ScreenShot).build());
     }
 
     @Test()

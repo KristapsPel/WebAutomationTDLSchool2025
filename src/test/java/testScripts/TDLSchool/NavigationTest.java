8069b5c5337487ff2ab9b0fe65aecf26b502dc45
Kristaps Pelcbergs <v-krpelc@microsoft.com>
2025-08-14 11:44:52 +0300

    Add NavigationTest with findElements

--- src/test/java/testScripts/TDLSchool/NavigationTest.java
+++ src/test/java/testScripts/TDLSchool/NavigationTest.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -18,23 +19,27 @@ public class NavigationTest {
     private WebDriver driver;
 
     @AfterMethod
-    private void tearDown () {
+    private void tearDown() {
         this.driver.close();
         this.driver.quit();
     }
 
+    @BeforeMethod
+    private void setUpBrowser() {
+        this.driver = setUpWebDriverManage("edge");
+        //this.driver.manage().window().maximize();
+        this.driver.get("https://tdlschool.com/");
+    }
+
     @Test
-    public void openTDLSchoolPage () throws InterruptedException {
+    public void openTDLSchoolPage() throws InterruptedException {
 
 //        WebDriver driver = setUpWebDriverManually("chrome");
 //        System.setProperty("wdm.edgeDriverUrl", "https://msedgedriver.microsoft.com/");
-        this.driver = setUpWebDriverManage("edge");
-        this.driver.manage().window().maximize();
-        this.driver.get("https://tdlschool.com/");
         Thread.sleep(2000);
 
         WebElement logo = driver.findElement(By.className("navigation__logo"));
-        WebElement buttonOurCourses= driver.findElement(By.linkText("Our courses"));
+        WebElement buttonOurCourses = driver.findElement(By.linkText("Our courses"));
         Assert.assertTrue(logo.isDisplayed(), "Logo is not displayed on page");
         Assert.assertTrue(buttonOurCourses.isDisplayed(), "Button is not displayed on page");
         WebElement careerPathsLink = driver.findElement(By.linkText("Career Paths"));
@@ -43,17 +48,33 @@ public class NavigationTest {
         Assert.assertEquals(careerPathTitle.getText(),
                 "Not sure where to start?",
                 "Title is not as expected");
-        System.out.println("Title:"+careerPathTitle.getText());
-        WebElement emailInputField= driver.findElement(By.name("email"));
+        System.out.println("Title:" + careerPathTitle.getText());
+        WebElement emailInputField = driver.findElement(By.name("email"));
         Assert.assertTrue(emailInputField.isDisplayed(), "Email input is not Displayed");
         emailInputField.sendKeys("test");
         Thread.sleep(1000);
 
         Thread.sleep(2000);
-        System.out.println("Title:"+ driver.getTitle());
-        System.out.println("URL:"+ driver.getCurrentUrl());
+        System.out.println("Title:" + driver.getTitle());
+        System.out.println("URL:" + driver.getCurrentUrl());
     }
-    private WebDriver setUpWebDriverManage (String browser) {
+
+    @Test()
+    public void checkUpcomingCoursesCount() throws InterruptedException {
+        System.out.println("Check if TDL School logo is Displayed");
+        boolean isLogoDisplayed = driver.findElement(By.className("navigation__logo")).isDisplayed();
+        Thread.sleep(5000);
+        Assert.assertTrue(isLogoDisplayed, "TDL School logo can not be found");
+        System.out.println("Check that 4 courses are displayed in upcoming section");
+        int countOfVisibleCourses = driver.findElements(
+                By.cssSelector("a.course-suggestions__course-card--active"))
+                .size();
+        Assert.assertEquals(countOfVisibleCourses, 4,
+                "Visible courses in Upcoming section is "+countOfVisibleCourses+
+                        " but expected was 9.");
+    }
+
+    private WebDriver setUpWebDriverManage(String browser) {
         WebDriver driver = null;
         switch (browser) {
             case "chrome":
@@ -71,11 +92,12 @@ public class NavigationTest {
         }
         return driver;
     }
-    private WebDriver setUpWebDriverManually (String browser) {
+
+    private WebDriver setUpWebDriverManually(String browser) {
         WebDriver driver = null;
-        switch (browser){
+        switch (browser) {
             case "chrome":
-                System.setProperty("webdriver.chrome.driver", "src"+ File.separator+
+                System.setProperty("webdriver.chrome.driver", "src" + File.separator +
                         "test" + File.separator +
                         "resources" + File.separator +
                         "drivers" + File.separator +
@@ -83,7 +105,7 @@ public class NavigationTest {
                 driver = new ChromeDriver();
                 break;
             case "firefox":
-                System.setProperty("webdriver.gecko.driver", "src"+ File.separator+
+                System.setProperty("webdriver.gecko.driver", "src" + File.separator +
                         "test" + File.separator +
                         "resources" + File.separator +
                         "drivers" + File.separator +
@@ -92,7 +114,7 @@ public class NavigationTest {
 
                 break;
             case "edge":
-                System.setProperty("webdriver.msedge.driver", "src"+ File.separator+
+                System.setProperty("webdriver.msedge.driver", "src" + File.separator +
                         "test" + File.separator +
                         "resources" + File.separator +
                         "drivers" + File.separator +
